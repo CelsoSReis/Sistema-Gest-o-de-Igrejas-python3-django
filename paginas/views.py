99c42a10b845c965470a8ecc085c98d8e0299402
Celso Reis <87393416+CelsoSReis@users.noreply.github.com>
2024-02-14 17:38:21 -0300

    Desenvolvendo Dashboard do sistema

--- paginas/views.py
+++ paginas/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
+
+#classe PaginaInicial extends TemplateView
 class PaginaInicial(TemplateView):
-    template_name = "paginas/index.html"
+    template_name = "paginas/modelo.html"
+
+
+class SobreView(TemplateView):
+    template_name = "paginas/sobre.html"
