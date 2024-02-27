e73d3a04ec9dc2d9c643ce8c184e9174f9f753b5
Celso Reis <87393416+CelsoSReis@users.noreply.github.com>
2024-02-26 22:54:59 -0300

    Desenvolvimento de modelo de banco de dados

--- cadastros/models.py
+++ cadastros/models.py
@@ -1,9 +1,34 @@
 from django.db import models
 
 # Create your models here.
+#IGREJAS
 class Igrejas(models.Model):
     nome = models.CharField(max_length=100, verbose_name='Nome')
     endereco = models.CharField(max_length=255, verbose_name='Endereço')
     email = models.EmailField(max_length=60, verbose_name='E-mail')
-    foto = models.ImageField()
-    telefone = models.CharField(max_length=20, verbose_name='Telefone')
+    telefone = models.IntegerField(verbose_name='Telefone')
+    def __str__(self): ##define método
+        return "{} ({})".format(self.nome, self.endereco, self.email, self.telefone)
+
+#CARGOS
+class Cargos(models.Model):
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+
+    def __str__(self): ##define método
+        return "{} ({})".format(self.nome)
+
+    
+#MEMBROS
+class Membros(models.Model):
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+    cpf = models.IntegerField(verbose_name='CPF')
+    data_nascimento = models.DateField(verbose_name='Data de Nascimento')
+    data_batismo = models.DateField(verbose_name='Data de Batismo')
+    endereco = models.CharField(max_length=255, verbose_name='Endereço')
+    email = models.EmailField(max_length=60, verbose_name='E-mail')
+    telefone = models.IntegerField(verbose_name='Telefone')
+    
+    cargos = models.ForeignKey(Cargos, on_delete=models.PROTECT)
+    
+    def __str__(self): ##define método
+        return "{} ({})".format(self.nome, self.endereco, self.email, self.telefone, self.cargos.nome)
