a81b395eccc050cb65b025188fd12a506f1c0e24
Celso Reis <87393416+CelsoSReis@users.noreply.github.com>
2024-02-27 22:02:38 -0300

    Registrando classes página admin

--- cadastros/models.py
+++ cadastros/models.py
@@ -5,50 +5,50 @@ from django.db import models
 # PASTOR PRESIDENTE
 class PastorPresidente(models.Model):
     nome = models.CharField(max_length=100, verbose_name='Nome')
-    cpf = models.IntegerField(verbose_name='CPF')
+    cpf = models.CharField(max_length=20, verbose_name='CPF')
     endereco = models.CharField(max_length=255, verbose_name='Endereço')
     email = models.EmailField(max_length=60, verbose_name='E-mail')
-    telefone = models.IntegerField(verbose_name='Telefone')
+    telefone = models.CharField(max_length=20, verbose_name='Telefone')
     def __str__(self): ##define método
-        return "{} ({})".format(self.nome,self.cpf, self.endereco, self.email, self.telefone)
+        return "{}".format(self.nome,self.cpf, self.endereco, self.email, self.telefone)
     
 # TESOUREIRO
 class Tesoureiro(models.Model):
     nome = models.CharField(max_length=100, verbose_name='Nome')
-    cpf = models.IntegerField(verbose_name='CPF')
+    cpf = models.CharField(max_length=20, verbose_name='CPF')
     endereco = models.CharField(max_length=255, verbose_name='Endereço')
     email = models.EmailField(max_length=60, verbose_name='E-mail')
-    telefone = models.IntegerField(verbose_name='Telefone')
+    telefone = models.CharField(max_length=20, verbose_name='Telefone')
     def __str__(self): ##define método
-        return "{} ({})".format(self.nome,self.cpf, self.endereco, self.email, self.telefone)
+        return "{}".format(self.nome,self.cpf, self.endereco, self.email, self.telefone)
     
 # SECRETÁRIOS
 class Secretarios(models.Model):
     nome = models.CharField(max_length=100, verbose_name='Nome')
-    cpf = models.IntegerField(verbose_name='CPF')
+    cpf = models.CharField(max_length=20, verbose_name='CPF')
     endereco = models.CharField(max_length=255, verbose_name='Endereço')
     email = models.EmailField(max_length=60, verbose_name='E-mail')
-    telefone = models.IntegerField(verbose_name='Telefone')
+    telefone = models.CharField(max_length=20, verbose_name='Telefone')
     def __str__(self): ##define método
-        return "{} ({})".format(self.nome,self.cpf, self.endereco, self.email, self.telefone)
+        return "{}".format(self.nome,self.cpf, self.endereco, self.email, self.telefone)
     
 # PASTORES
 class Pastores(models.Model):
     nome = models.CharField(max_length=100, verbose_name='Nome')
-    cpf = models.IntegerField(verbose_name='CPF')
+    cpf = models.CharField(max_length=20, verbose_name='CPF')
     endereco = models.CharField(max_length=255, verbose_name='Endereço')
     email = models.EmailField(max_length=60, verbose_name='E-mail')
     data_nascimento = models.DateField(verbose_name='Data de Nascimento')
-    telefone = models.IntegerField(verbose_name='Telefone')
+    telefone = models.CharField(max_length=20, verbose_name='Telefone')
     def __str__(self): ##define método
-        return "{} ({})".format(self.nome,self.cpf, self.endereco, self.email, self.telefone)
+        return "{}".format(self.nome,self.cpf, self.endereco, self.email, self.telefone)
 
 # IGREJAS
 class Igrejas(models.Model):
     nome = models.CharField(max_length=100, verbose_name='Nome')
     endereco = models.CharField(max_length=255, verbose_name='Endereço')
     email = models.EmailField(max_length=60, verbose_name='E-mail')
-    telefone = models.IntegerField(verbose_name='Telefone')
+    telefone = models.CharField(max_length=20, verbose_name='Telefone')
 
     pastores = models.ForeignKey(Pastores, on_delete=models.PROTECT)
 
@@ -60,18 +60,18 @@ class Cargos(models.Model):
     nome = models.CharField(max_length=100, verbose_name='Nome')
 
     def __str__(self): ##define método
-        return "{} ({})".format(self.nome)
+        return "{} ()".format(self.nome)
 
     
 #MEMBROS
 class Membros(models.Model):
     nome = models.CharField(max_length=100, verbose_name='Nome')
-    cpf = models.IntegerField(verbose_name='CPF')
+    cpf = models.CharField(max_length=20, verbose_name='CPF')
     data_nascimento = models.DateField(verbose_name='Data de Nascimento')
     data_batismo = models.DateField(verbose_name='Data de Batismo')
     endereco = models.CharField(max_length=255, verbose_name='Endereço')
     email = models.EmailField(max_length=60, verbose_name='E-mail')
-    telefone = models.IntegerField(verbose_name='Telefone')
+    telefone = models.CharField(max_length=20, verbose_name='Telefone')
     
     cargos = models.ForeignKey(Cargos, on_delete=models.PROTECT)
     
