f9bc5abe3938ff632d8c197cdc1c70b97e0b2932
Celso Reis <celsosr87@gmail.com>
2025-01-21 17:54:34 -0300

    Model - Cadastro de Membros
    Desenvolvimento da Model de Cadastro de Membros

--- plataformaigreja/models.py
+++ plataformaigreja/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Membros(models.Model):
+    choices_sexo = (('F', 'Feminino'),
+                    ('M', 'Maculino'))
+    nome = models.CharField(max_length=50)
+    sexo = models.CharField(max_length=1, choices=choices_sexo)
+    cpf = models.CharField(max_length=20)
+    idade = models.IntegerField()
+    email = models.EmailField()
+    telefone = models.CharField(max_length=19)
+    endereco = models.CharField(max_length=100)
+    foto = models.ImageField(upload_to="img_membros", null=True, blank=True)
+    data_cadastro = models.DateField()
+    data_nascimento = models.DateField()
+    obs = models.TextField(max_length=200)
+    data_batismo = models.DateField()
+    cargo = models.CharField(max_length=50)
+    ativo = models.CharField(max_length=6)
+    pastor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
--- plataformaigreja/views.py
+++ plataformaigreja/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(login_url='/usuarios/login')
 def membros(request):
-    return render(request, 'plataformaigreja.html')
+    return render(request, 'membros.html')
