e286feb37dfc603daabb15407a4a7888d1a5cf93
Celso Reis <celsosr87@gmail.com>
2025-02-02 23:56:48 -0300

    View Editar Dízimos

--- plataformadizimos/urls.py
+++ plataformadizimos/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import dizimos, editar_dizimo
 
 urlpatterns = [
     path('dizimos/', views.dizimos, name='dizimos'),
+    path('dizimos/editar/<int:dizimo_id>/', editar_dizimo, name='editar_dizimo'),
 ]
--- plataformadizimos/views.py
+++ plataformadizimos/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.contrib.messages import constants
 from .models import Dizimos, Membros
+from django.http import JsonResponse
 
 
-
-# Create your views here.
+# Registrar dízimo.
 @login_required(login_url='/usuarios/login')
 def dizimos(request):
     if request.method == "POST":
@@ -28,3 +27,26 @@ def dizimos(request):
     dizimos = Dizimos.objects.filter(membro__pastor=request.user).order_by("-data_dizimo")
 
     return render(request, "dizimos.html", {"membros": membros, "dizimos": dizimos})
+
+@login_required(login_url='/usuarios/login')
+def editar_dizimo(request, dizimo_id):
+    dizimos = get_object_or_404(Dizimos, id=dizimo_id, membro__pastor=request.user)
+
+    if request.method == "POST":
+        membro_id = request.POST.get("membro")
+        valor = request.POST.get("valor")
+
+        if not membro_id or not valor:
+            messages.error(request, "Preencha todos os campos.")
+            return redirect("/dizimos/")
+
+        membro = get_object_or_404(Membros, id=membro_id, pastor=request.user)
+
+        dizimos.membro = membro
+        dizimos.valor = valor
+        dizimos.save()
+
+        messages.success(request, "Dízimo atualizado com sucesso!")
+        return redirect("/dizimos/")
+
+    return JsonResponse({"error": "Método inválido"}, status=400)
