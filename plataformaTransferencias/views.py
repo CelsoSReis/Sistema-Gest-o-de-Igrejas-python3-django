e64ae25f3852221fd89de81dbe86407262440810
Celso Reis <celsosr87@gmail.com>
2025-05-01 17:39:59 -0300

    Atualização página transferencia de membros

--- plataformaTransferencias/views.py
+++ plataformaTransferencias/views.py
@@ -10,36 +10,7 @@ from django.contrib.auth.decorators import login_required
 def listar_transferencias(request):
     transferencias = Transferencia.objects.all()
     return render(request, 'transferencias/listar_transferencias.html', {'transferencias': transferencias})
-"""
-@login_required(login_url='/usuarios/login')
-def transferir_membro(request, membro_id):
-    membro = get_object_or_404(Membros, id=membro_id)
-    
-    # Pega a igreja que o usuário logado é o pastor
-    igreja_origem = get_object_or_404(Igreja, pastor=request.user)
 
-    if request.method == "POST":
-        nome_igreja_destino = request.POST.get('igreja_destino')
-
-        # Cria uma nova igreja no banco de dados ou busca se já existir
-        igreja_destino, created = Igreja.objects.get_or_create(nome_igreja=nome_igreja_destino)
-
-        Transferencia.objects.create(
-            membro=membro,
-            igreja_origem=igreja_origem,
-            igreja_destino=igreja_destino,
-            data_transferencia=timezone.now()
-        )
-
-        membro.delete()  # Remove o membro após confirmar a transferência
-        messages.success(request, 'Membro transferido com sucesso!')
-        return redirect('listar_transferencias')
-
-    return render(request, 'transferencias/transferir_membro.html', {
-        'membro': membro,
-        'igreja_origem': igreja_origem,
-    })
-"""
 # Função para realizar transferência de membro / após transferência de membro o mesmo fica inativo no banco
 @login_required(login_url='/usuarios/login')
 def transferir_membro(request, membro_id):
@@ -50,9 +21,9 @@ def transferir_membro(request, membro_id):
         membro.save()
 
         messages.success(request, f'{membro.nome} foi marcado como transferido (inativo).')
-        return redirect('controle_transf')
+        return redirect('/index/membros/')
 
-    return redirect('controle_transf')
+    return redirect('/index/membros/')
 
 #Controle de membros transferidos
 @login_required(login_url='/usuarios/login')
